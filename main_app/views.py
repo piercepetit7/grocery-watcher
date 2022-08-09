5a3354661d374b518679282b8e21cae8369093fc
Pierce Petit <ppetit2496@gmail.com>
2022-08-09 08:53:18 -0500

    stub up about

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Watchers</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About Grocery Watchers</h1>')
